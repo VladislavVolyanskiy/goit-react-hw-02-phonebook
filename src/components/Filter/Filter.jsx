521f8fd68184708c32ba53f61dbe2d7ce8b036ef
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2023-03-29 00:45:22 +0300

    props fixed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -12,6 +12,6 @@ export const Filter = ({ value, onChange }) => {
 };
 
 Filter.propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
